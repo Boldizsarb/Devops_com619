0711a8d3772536c234642c53b8542b0e8514ac25
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2023-12-05 14:01:57 +0000

    New domain and certificate installed on Azure

--- deploy_script.sh
+++ deploy_script.sh
@@ -12,13 +12,14 @@ docker-compose down
 # Start the stack as defined in docker-compose.yml
 docker-compose up -d
 
-# Wait for the services to start
-sleep 10
 
 # Run Certbot to obtain certificates, if needed
 # Uncomment the following lines if you need to obtain certificates
-docker-compose run --rm certbot certonly --standalone --preferred-challenges http --agree-tos --email devops_com619@hotmail.com -d com619trial.uksouth.cloudapp.azure.com
-# sleep 2
+docker-compose run --rm certbot certonly --standalone --preferred-challenges http --agree-tos --email 5giwao61@solent.ac.uk -d com619devops.uksouth.cloudapp.azure.com
+
+
+# Wait for the services to start
+sleep 10
 
 # Reload NGINX configuration to ensure it uses the obtained certificates
 docker-compose exec nginx nginx -s reload
