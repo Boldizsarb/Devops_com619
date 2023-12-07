ab6e20a4aba95523309d6e09a065da31d32b2ae2
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2023-12-07 20:05:42 +0000

    Error fix

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,34 +3,21 @@
 # Navigate to the directory containing your docker-compose.yml file
 cd "$(dirname "$0")"
 
-# Pull the latest version of your application image
-docker pull dapsonic/devops_com619-devops:latest
+# Pull the latest images
+docker-compose pull
 
-# Pull the MySQL image
-docker pull mysql:5.7
+# Stop and remove any previous containers
+docker-compose down
 
-# Stop existing containers (if any)
-docker stop myapp-db || true
-docker rm -f myapp-db || true
-docker stop myapp || true
-docker rm -f myapp || true
+# Start the services defined in docker-compose.yml
+docker-compose up -d
 
-# Start MySQL container
-docker run -d --name myapp-db -e MYSQL_ROOT_PASSWORD=devops -e MYSQL_DATABASE=myappdb -p 3306:3306 mysql:5.7
+# Wait for the services to start
+sleep 10
 
-# Start your application container
-# Make sure to link it to the MySQL container or provide database connection information
-docker run -d --name myapp --link myapp-db:mysql -p 80:80 dapsonic/devops_com619-devops:latest
+# Run Certbot to obtain certificates, if needed
+# Note: Make sure the nginx service is configured to serve the ACME challenge
+docker-compose run --rm certbot certonly --standalone --preferred-challenges http --agree-tos --email 5giwao61@solent.ac.uk -d comdevops.uksouth.cloudapp.azure.com
 
-
-# Run Certbot container to obtain certificates
-docker run --rm \
-    -v "/etc/letsencrypt:/etc/letsencrypt" \
-    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-    -p 80:80 \
-    certbot/certbot certonly \
-    --standalone \
-    --preferred-challenges http \
-    --agree-tos \
-    --email 5giwao61@solent.ac.uk \
-    -d comdevops.uksouth.cloudapp.azure.com
+# Reload NGINX configuration to ensure it uses the obtained certificates
+docker-compose exec nginx nginx -s reload
