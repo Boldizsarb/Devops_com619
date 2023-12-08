8f80d6c7793b165b3a30934aa343bab25a0c48c3
Oladapo Giwa-Daramola <91552016+Oladapo01@users.noreply.github.com>
2023-12-08 21:39:04 +0000

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Pull the latest version of your application image
 docker pull dapsonic/devops_com619-devops:latest
@@ -7,26 +8,40 @@ docker pull dapsonic/devops_com619-devops:latest
 docker pull mysql:5.7
 
 # Stop existing containers (if any)
-docker stop myapp-db || true
-docker rm -f myapp-db || true
-docker stop myapp || true
-docker rm -f myapp || true
+docker stop myapp-db || true && docker rm myapp-db || true
+docker stop myapp || true && docker rm myapp || true
 
 # Start MySQL container
 docker run -d --name myapp-db -e MYSQL_ROOT_PASSWORD=devops -e MYSQL_DATABASE=myappdb -p 3306:3306 mysql:5.7
 
 # Start your application container
-# Make sure to link it to the MySQL container or provide database connection information
-docker run -d --name myapp --link myapp-db:mysql -p 80:80 dapsonic/devops_com619-devops:latest
+docker run -d --name myapp --link myapp-db:mysql -p 3000:3000 dapsonic/devops_com619-devops:latest
 
 # Wait for the services to start
 sleep 10
 
-# Run Certbot to obtain certificates, if needed
-docker run --rm -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" -p 80:80 --name certbot certbot/certbot certonly --standalone --preferred-challenges http --agree-tos --email 5giwao61@solent.ac.uk -d comdevops.uksouth.cloudapp.azure.com
+# Stop Nginx if it's running to free up port 80
+docker stop nginx || true && docker rm nginx || true
 
-# Reload NGINX configuration to ensure it uses the obtained certificates
-docker exec nginx dapsonic/nginx:latest -s reload
+# Run Certbot container to obtain certificates
+docker run --rm \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -p 80:80 \
+  certbot/certbot certonly \
+  --standalone \
+  --preferred-challenges http \
+  --agree-tos \
+  --email 5giwao61@solent.ac.uk \
+  -d comdevops.uksouth.cloudapp.azure.com
 
-# Reload NGINX configuration to ensure it uses the obtained certificates
-docker exec nginx nginx -s reload
+# Start Nginx container with mounted volumes for SSL
+docker run -d --name nginx \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf" \
+  -p 80:80 \
+  -p 443:443 \
+  nginx:latest
+
+# The Certbot container will exit, so no need to reload Nginx in this script
