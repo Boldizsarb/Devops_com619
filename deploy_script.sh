e41e491cc70058a3b8b82545d8aa2d0fe257b803
Oladapo01 <91552016+Oladapo01@users.noreply.github.com>
2023-12-12 15:40:48 +0000

    Deployment

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,48 +1,25 @@
 #!/bin/bash
 set -e
 
-# Pull the latest version of your application image
-docker pull dapsonic/devops_com619-devops:latest
-docker pull dapsonic/mynginx:latest
+# Environment variables
+IMAGE_APP="dapsonic/devops_com619-devops:latest"
+IMAGE_NGINX="dapsonic/devops_com619-nginx:latest"
 
-# Pull the MySQL image
-docker pull mysql:5.7
+# Pull the latest version of your application and NGINX images
+docker pull $IMAGE_APP
+docker pull $IMAGE_NGINX
 
-# Stop existing containers (if any)
-docker stop myapp-db || true && docker rm myapp-db || true
-docker stop myapp || true && docker rm myapp || true
+# Stop and remove existing containers
 
-# Start MySQL container
-docker run -d --name myapp-db -e MYSQL_ROOT_PASSWORD=devops -e MYSQL_DATABASE=myappdb -p 3306:3306 mysql:5.7
+docker stop myapp nginx || true
+docker rm myapp nginx || true
+
 
 # Start application container
-docker run -d --name myapp --link myapp-db:mysql -p 3000:3000 dapsonic/devops_com619-devops:latest
+docker run -d --name myapp --network=devops-net --restart unless-stopped -p 3000:3000 $IMAGE_APP
 
-# Start the Nginx container
-docker run -d --name nginx -p 80:80 -p 443:443 dapsonic/nginx:latest
-
-# Wait for the services to start
-sleep 10
-
-# Stop Nginx if it's running to free up port 80
-#docker stop nginx || true && docker rm nginx || true
-
-# Run Certbot container to obtain certificates
-#docker run --rm \
-#  -v "/etc/letsencrypt:/etc/letsencrypt" \
-#  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
-#  -p 80:80 \
-#  certbot/certbot certonly \
-#  --standalone \
-#  --preferred-challenges http \
-#  --agree-tos \
-#  --email 5giwao61@solent.ac.uk \
-#  -d comdevops.uksouth.cloudapp.azure.com
-
-# Start Nginx container with mounted volumes for SSL
-docker run -d --name nginx -p 80:80 -p 443:443 \
-  -v "/etc/letsencrypt:/etc/letsencrypt" \
-  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+# Start NGINX container
+docker run -d --name nginx --network=devops-net --restart unless-stopped -p 80:80 -p 443:443 \
+  -v "/etc/letsencrypt:/etc/letsencrypt/" \
   -v "/home/com619/nginx.conf:/etc/nginx/nginx.conf" \
-  nginx:latest
-
+  $IMAGE_NGINX
