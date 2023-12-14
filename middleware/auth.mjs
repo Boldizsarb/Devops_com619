85e1f6074c76ce6cac8b59e33172905ffa6cf57e
Cloud User <com619@com619.vcjkaqpc4qsetmtoeetvqsjnue.zx.internal.cloudapp.net>
2023-12-14 22:34:09 +0000

    debug msg failing

--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -1,27 +1,28 @@
 import userModel from '../Models/userModel.mjs';
 export const isAuthenticated = (req, res, next) => {
-    console.log(`i am here ${req.session.username}`);
+    console.log(`i am here !!!!!!!!!!!!!!!!!!!!! ${req.session.username}`);
     if (req.session.username) {
-        console.log(`i am here ${req.session.username}`);
-        return next();
+        console.log(`i am here 2 !!!!!!!!!!!!!!!!!!!!!!!!!!! ${req.session.username}`);
+        return next();`
     }
     res.status(403).send({ msg: 'You are not authorized to view this page!' });
 };
 export const isAdmin = (req, res, next) => {
-    if (req.session.username) {
-        const userInstance = new userModel();
-        userInstance.getUserByUsername(req.session.username).then((user) => {
-            if (user && user.permission_level === 'admin') {
-                return next();
-            } else {
-                return res.status(403).send({ msg: 'You are not authorized to view this page!' });
-            }
-        }).catch((error) => {
-            return res.status(500).send({ msg: 'Internal server error' });
-        });
-    } else {
-        return res.status(403).send({ msg: 'You are not authorized to view this page!' });
-    }
+	console.log(`i am here 3 !!!!!!!!!!!!!!!!!!!!!!!!!!! ${req.session.username}`);
+	if (req.session.username) {
+        	const userInstance = new userModel();
+		userInstance.getUserByUsername(req.session.username).then((user) => {
+			if (user && user.permission_level === 'admin') {
+				return next();
+			} else {
+				return res.status(403).send({ msg: 'You are not authorized to view this page!' });
+			}
+			}).catch((error) => {
+				return res.status(500).send({ msg: 'Internal server error' });
+			});
+	} else {
+		return res.status(403).send({ msg: 'You are not authorized to view this page!' });
+	}
 };
 export const sessionsAuth = (req, res, next) => {
     if (req.session.username) {
--- public/scripts/common.js
+++ public/scripts/common.js
@@ -240,15 +240,7 @@ function SideBar({ verifyLogin, loggedInUser, isLoggedIn }) {
           <span>Swagger API</span>
         </a>
       </li>
-
-      <hr className="sidebar-divider" />
-      <li className="nav-item">
-        <a id="Monitoring" className="nav-link" href="https://my.pingdom.com/app/reports/uptime#check=12833055&daterange=7days&tab=result_tab&checkName=Azure%20Cloudapp%20Uksouth%20Comdevops">
-          <i className="fas fa-fw fa-wrench"></i>
-          <span>Monitoring</span>
-        </a>
-      </li>
-
+ 
       <hr className="sidebar-divider" />
       <li className="nav-item">
         <a id="Apache Licence" className="nav-link" href="https://comdevops.uksouth.cloudapp.azure.com/public/apache.html">
